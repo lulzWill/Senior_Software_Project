ba0c5eee71caa05d49fb2aec70d9bf08659525b3
jarredhutch <jarred-hutchison@uiowa.edu>
2016-02-08 18:40:33 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def homepage
+    end
+    
+    def show
+        
+    end
+    
+    def index
+        
+    end
+    
+    def create
+        
+    end
+    
+    def new
+    
+    end
 end
+

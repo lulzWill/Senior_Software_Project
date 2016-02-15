8f6e4b02edddff118b6335cfa87f5d37bd8f3f69
konefes <konefes@gmail.com>
2016-02-15 18:59:17 +0000

    added review update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,9 @@
 class ReviewsController < ApplicationController
     
+    def review_params
+        params.require(:review).permit(:rating, :comment)
+    end
+    
     def show
         id = params[:id]
         @review = Review.find(id)
@@ -14,5 +18,11 @@ class ReviewsController < ApplicationController
         id = params[:id]
         @review = Review.find(id)
         @location = Location.find(@review.location_id)
+    end
+    
+    def update
+        @review = Review.find params[:id]
+        @review.update_attributes!(review_params)
+        redirect_to review_path(@review)
     end
 end

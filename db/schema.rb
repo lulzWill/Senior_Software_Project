c91ffa71287e2785a62919fb4740206038e5a172
jarredhutch <jarred-hutchison@uiowa.edu>
2016-02-17 18:56:08 +0000

    finalized schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20160212162250) do
   end
 
   create_table "users", force: :cascade do |t|
-<<<<<<< HEAD
     t.string   "email"
     t.string   "user_id"
     t.string   "password_digest"
@@ -73,10 +72,6 @@ ActiveRecord::Schema.define(version: 20160212162250) do
     t.string   "session_token"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-=======
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
->>>>>>> 1fcb9fabcf7c58e3ca04dc0cd3846cc4ed4b82be
   end
 
   create_table "visits", force: :cascade do |t|

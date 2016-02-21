e080a945229827c6c193207d49029a359945ece2
konefes <konefes@gmail.com>
2016-02-21 22:32:52 +0000

    finished unit testing reviews, visits, locations

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-    
-    factory :user do
-        user_id "fake"
-        email  "a@b.com"
-        password  "fakepass1"
-    end
-
-end
--- spec/factories/visits.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-    
-    factory :visit do
-        user_id "fake"
-        start_date  "2000-1-1"
-        end_date  "2000-1-2"
-    end
-
-end

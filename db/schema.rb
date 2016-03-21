4170d4a0b7c7841339f563f9aeffba935c726c55
konefes <konefes@gmail.com>
2016-03-21 22:07:16 +0000

    created activities table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212162250) do
+ActiveRecord::Schema.define(version: 20160321164500) do
+
+  create_table "activities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "username"
+    t.string   "type"
+    t.text     "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "albums", force: :cascade do |t|
     t.integer  "user_id"

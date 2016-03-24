b63fb8f37f6279f4c7834c7e2aff88431299418c
ryantatum <ryant9211@gmail.com>
2016-03-24 01:24:59 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,8 @@ class UsersController < ApplicationController
     end
     
     def index
-        
+      @users = User.where.not("id = ?",current_user.id).order("created_at DESC")
+      @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def user_params

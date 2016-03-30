db4f14f83e52e4969bb6a70ca04bbab9072a0425
jarredhutch <jarred-hutchison@uiowa.edu>
2016-03-30 16:16:12 +0000

    little map details

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -107,8 +107,10 @@ function initAutocomplete()
 
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
+  var input2 = document.getElementById('floating-panel');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input2);
   
   
   

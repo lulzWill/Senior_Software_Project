e68c2a6c6b579ebc34702ae90148825878137cd2
ryantatum <ryant9211@gmail.com>
2016-04-02 20:03:18 +0000

    aws stuff

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => 'AKIAIX3WWZM57ARACQIA',                        # required
+    :aws_secret_access_key  => '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',                     # required
+    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :host                   => 's3.example.com',             # optional, defaults to nil
+    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'backpacktraveler'                             # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end

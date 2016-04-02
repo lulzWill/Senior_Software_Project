897928e46a98b554c8ac4ade0a3426d9ac377a4c
ryantatum <ryant9211@gmail.com>
2016-04-02 20:15:28 +0000

    aws changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAIX3WWZM57ARACQIA',                        # required
     :aws_secret_access_key  => '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',                     # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :region                 => 'us-east-1',                  # optional, defaults to 'us-east-1'
     :host                   => 's3.example.com',             # optional, defaults to nil
     :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }

e50653867246cf8fc7072648d82ec35b9171e595
ryantatum <ryant9211@gmail.com>
2016-04-02 21:07:32 +0000

    fixed keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,9 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIX3WWZM57ARACQIA',                        # required
+    :aws_access_key_id      => 'AKIAIBGGY32ISHRDIASQ',                        # required
     :aws_secret_access_key  => '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',                     # required
-    :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
+    :region                 => 'us-west-2'                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'backpacktraveler'                             # required
   config.fog_public     = false                                   # optional, defaults to true
--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -2,11 +2,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIX3WWZM57ARACQIA',                        # required
-    aws_secret_access_key: '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',                        # required
-    region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-    host:                  's3.example.com',             # optional, defaults to nil
-    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    aws_access_key_id:     'AKIAIBGGY32ISHRDIASQ',                        # required
+    aws_secret_access_key: 'KppN/NO1a+0Mxa0A1wkqIY9a0WqPwSomWQ7Qqyg5',                        # required
+    region:                'us-west-2'                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'backpacktraveler'                          # required
   config.fog_public     = false                                        # optional, defaults to true

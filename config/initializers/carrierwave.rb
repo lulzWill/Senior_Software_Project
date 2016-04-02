a15d53b3c7e29624a58e2d6301ffdb9adca77f7e
ryantatum <ryant9211@gmail.com>
2016-04-02 21:36:35 +0000

    region change aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAIX3WWZM57ARACQIA',                        # required
     :aws_secret_access_key  => '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',                     # required
-    :region                 => 'us-west-2'                  # optional, defaults to 'us-east-1'
+    :region                 => 'us-east-1'                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'backpacktraveler'                             # required
   config.fog_public     = false                                   # optional, defaults to true

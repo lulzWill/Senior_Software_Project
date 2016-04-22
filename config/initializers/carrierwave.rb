9f944a473a88d8115a0b2ca5ccc761d65864600c
ryantatum <ryant9211@gmail.com>
2016-04-22 04:08:18 +0000

    fix aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIX3WWZM57ARACQIA',                        # required
-    :aws_secret_access_key  => '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',                     # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
     :region                 => 'us-east-1'                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'backpacktraveler'                             # required

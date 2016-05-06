90fc24b7efbe620febf6aef1ab8f668409acbef8
konefes <konefes@gmail.com>
2016-05-06 15:23:29 +0000

    added aws new key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,10 +4,11 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
+    :aws_access_key_id      => "AKIAJEPHBMVTXIX6RSWA",                        # required
+    :aws_secret_access_key  => "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",                     # required
     :region                 => 'us-east-1'                  # optional, defaults to 'us-east-1'
   }
+  
   config.fog_directory  = 'backpacktraveler'                             # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
